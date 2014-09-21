9a611077fdbac2a9ac96c2a34d18670ca86192eb
Lucas Silveira Melo <lucas.silveiramelo@gmail.com>
2014-09-20 22:01:25 -0300

    Adicao do arquivo topico_1.py ao repositorio

---
+++ topico_1.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Hello world em python
+print 'Hello world!'
+
+# Declarando variáveis em python
+mensagem = 'Esta é uma string em python'
+print mensagem
+
+a = 1
+b = 10.0
+c = a + b
+print c
+
+print 'a = ' + str(a) + ' b = ' + str(b) + ' c = ' + str(c)
+
+print '%d %f %f' % (a, b, c)
+
+a = True
+b = False
+
+if a:
+	print 'A variavel a é True'
+else:
+	print 'A variável a é falsa'
+
